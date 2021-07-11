5adba5cdcd3336d99914825657cde318a7f527fd
Zhe Wang <zhewang010101@gmail.com>
2021-07-11 19:51:09 -0400

    Create activities for 4 group members

--- app/src/main/java/algonquin/cst2335/final_project/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/final_project/MainActivity.java
@@ -2,7 +2,9 @@ package algonquin.cst2335.final_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button DiYuBtn = findViewById(R.id.DiYu);
+        Button XiangqianCheBtn = findViewById(R.id.XiangqianChe);
+        Button ZheWangBtn = findViewById(R.id.ZheWang);
+        Button ZhiqianQuBtn = findViewById(R.id.ZhiqianQu);
+
+        DiYuBtn.setOnClickListener( v -> {
+            startActivity(new Intent(MainActivity.this, DiYu.class));
+        } );
+        XiangqianCheBtn.setOnClickListener( v -> {
+            startActivity(new Intent(MainActivity.this, XiangqianChe.class));
+        } );
+        ZheWangBtn.setOnClickListener( v -> {
+            startActivity(new Intent(MainActivity.this, ZheWang.class));
+        } );
+        ZhiqianQuBtn.setOnClickListener( v -> {
+            startActivity(new Intent(MainActivity.this, ZhiqianQu.class));
+        } );
     }
 }
